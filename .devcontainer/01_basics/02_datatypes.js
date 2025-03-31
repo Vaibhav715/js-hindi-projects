47b90c18f69d3394b4d26d82d4182d28a7c97a66
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2025-03-31 15:37:33 +0000

    datatypes done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all code as JS newer version
+// alert(3+5) //we are using nodeJS not browser
+// code readiability should be high 
+let name ="Vaibhav"  //string
+let age = 22         //integer
+let isLoggedIn = true//boolean
+let stateA; //undefined
+let stateB=null; //khali(kuch bhi nhi h isme) value h 
+//it is defined intensionally
+   
+/* Primitive data types  */
+
+/*
+
+number => 2 to power of 53
+bigint used in websites like stockmacketing,trading,insta etc
+string => "" preferable to use double quotes
+boolean => true/false
+null=> standalone value
+undefined => something which is not defined yet
+symbol => for showing uniqueness in component in reactjs
+
+*/
+
+//object
+console.log(typeof "Vaibhav");
+console.log(typeof age);
+// to know datatype of variable
+console.log(typeof null); // results in object
+console.log(typeof undefined); // results in undefined 
