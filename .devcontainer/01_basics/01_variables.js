0e3a35ef9ec65309190a3f28e712b25b494d78f7
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2025-03-31 14:52:03 +0000

    changes done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountName = 2341414
+let accountEmail = "vaib715harry@gmail.com"
+var accountpassword = "12345"
+accountCity = "Varanasi"
+ let accountState; 
+// uninialised variable is resulted as undefined
+// accountName = 771503
+ 
+console.log(accountName);
+accountEmail = "vabhavsrivastava7715@gmail.com"
+accountpassword = 2341424
+accountCity = "Mumbai"
+console.table([accountpassword,accountEmail,accountName,accountCity,accountState]);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
