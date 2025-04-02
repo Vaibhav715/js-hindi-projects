43c7bce867e4e8c9bf6ccd4fc714e8250ce2f329
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2025-04-02 14:18:34 +0000

    conversion study done

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,51 @@
+let score = "77"
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let ValueInNumber = Number(score)
+// console.log(typeof(ValueInNumber))
+
+// Conversion from different datatype to number
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// "abc" => NaN
+// null => 0
+// true => 1 or false => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanLoggedIn);
+// console.log(booleanLoggedIn);
+
+// Conversion   
+// 1 => true,0=> false
+// "" => false
+// "Vaibhav" => true
+
+// *************Operations***************
+
+let value = 2
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+let str1 = "konnichiwa"
+let str2 =" Vaibhav "
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+
+
