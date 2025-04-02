9b9c51852e69d11f8f34890bad61d12bb7996657
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2025-04-02 15:05:45 +0000

    changes done

---
+++ .devcontainer/01_basics/04_comparisons.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+// ****************Avoid using "null" and "undefined" for comparison *************************
+// console.log(null == 0);  =>  false
+// console.log(null >= 0);  =>  true 
+// console.log(null < 0);   =>  false 
+
+// console.log(undefined == 0); => gives false in every case
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === strict checking don't convert different datatypes
+console.log("2" === 2);
+
---
+++ .devcontainer/datatypeSummary.js
@@ -0,0 +1,15 @@
+// ************Inteview related datatype summary JS*****
+
+// Primitive datatype(Call by value)
+
+// 7 types => string,number,boolean,null,undefined,symbol,BigInt(very big values)
+
+// Reference(Non-primitive)
+
+// Array,Objects,Functions
+// const score; => Error #we have to give intial value to score if it is  defined as constant
+
+const id = Symbol("2341414")
+const anotherId = Symbol("2341414")
+
+console.log(id === anotherId);
