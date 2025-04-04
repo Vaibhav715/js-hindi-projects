054f19cc7914dee68ad67190c437ae1c3e2c02ee
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2025-04-04 15:04:22 +0000

    changes done

--- .devcontainer/datatypeSummary.js
+++ .devcontainer/datatypeSummary.js
@@ -3,13 +3,29 @@
 // Primitive datatype(Call by value)
 
 // 7 types => string,number,boolean,null,undefined,symbol,BigInt(very big values)
+const id = Symbol("2341414")
+const anotherId = Symbol("2341414")
+
+// console.log(id === anotherId);
+// console.log(typeof(id));
+const bigNumber = 2354648774564n
 
 // Reference(Non-primitive)
 
 // Array,Objects,Functions
 // const score; => Error #we have to give intial value to score if it is  defined as constant
 
-const id = Symbol("2341414")
-const anotherId = Symbol("2341414")
+// Declaration of Array
+const heros =["Ben 10","Naruto","Luffy","All_Might"];
 
-console.log(id === anotherId);
+// Declaration of Object
+let my_Obj={
+    name:"Vaibhav",
+    age:21
+}
+
+// Declaration of function
+const myFunction = function () {
+    console.log("Hello World !");
+    
+}
