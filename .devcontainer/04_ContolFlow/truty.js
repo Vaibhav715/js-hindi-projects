1cb7414230787267f3561114f96d34a5b2414667
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2025-11-16 17:51:59 +0000

    truthy and falsy valus done

---
+++ .devcontainer/04_ContolFlow/truty.js
@@ -0,0 +1,21 @@
+// const UserEmail = "Vaibhav@Sri.ai"
+//  if (UserEmail) {
+//     console.log("Email is valid");
+//  } else {
+//     console.log("Email is not valid");
+//  }
+
+//  const Arr1 = [1,2,3,4,5];
+//  if (Arr1.length > 0) {
+//     console.log("Array is not empty");
+//  } else {
+//     console.log("Array is empty");
+//  }
+
+const emptyObjesct = {};
+
+if (Object.keys(emptyObjesct).length === 0) {
+    console.log("Object is empty");
+} else {
+    console.log("Object is not empty");
+}
